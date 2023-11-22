39d47437f943ab4d749607d1d450043a339c51e2
JerryWang0122 <school832@gmail.com>
2023-11-22 11:35:14 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ app.get('/', (req, res) => {
   res.send('express app for URL shortener')
 })
 
+app.get('/shorten', (req, res) => {
+  res.send('provide shorten URL')
+})
+
+app.get('/shorten/:shortURL', (req, res) => {
+  const shortURL = req.params.shortURL
+  res.send(`decode shortURL: ${shortURL}, and go to original website`)
+})
+
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
 })
