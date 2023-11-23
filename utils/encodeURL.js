2f4c19fdad3024da0f90a7d6b8adb1823ba13a7a
JerryWang0122 <school832@gmail.com>
2023-11-23 19:16:42 +0800

    feat: use fs package to establish database We can now utilize a database to verify the existance of a shortened URL or updata it accordingly

--- utils/encodeURL.js
+++ utils/encodeURL.js
@@ -1,3 +1,7 @@
+const fs = require("fs")
+
+const filePath = './public/jsons/datas.json'
+
 // 字元表：a-z, A-Z, 0-9
 const CHAR_TABLE = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 const MAX = CHAR_TABLE.length - 1
@@ -6,11 +10,35 @@ const MIN = 0
 const DIGIT = 5
 
 module.exports = (originalURL) => {
-  return generateShortURL()
+  // 當網址以"/"結尾時，移除最後字元
+  if (originalURL.endsWith("/")) originalURL = originalURL.slice(0, -1)
+
+  // Generate by ChatGPT
+  // using synchronous
+  try {
+    const data = fs.readFileSync(filePath, 'utf8')
+    const jsonData = data ? JSON.parse(data) : {}
+
+    if (jsonData[originalURL]){ // 如果已經簡化過，則直接回傳
+      return jsonData[originalURL]
+    } else {  // 若尚無資料
+      // generate shorten URL until there's no repetitive
+      let shortenURL = generateShortURL()
+      while(Object.values(jsonData).includes(shortenURL)) {
+        shortenURL = generateShortURL()
+      }
+      // update jsonData and write into database
+      jsonData[originalURL] = shortenURL
+      writeToDatabase(jsonData, filePath)
+      return shortenURL
+    }
+  } catch (err) {
+    console.error('Error reading or parsing JSON file:', err)
+  }
 }
 
 function generateShortURL() {
-  // 儲存回傳的短網址
+  // 儲存欲回傳的短網址
   let shortURL = ''
 
   for (let i = 0; i < DIGIT; i++) {
@@ -19,4 +47,14 @@ function generateShortURL() {
   }
 
   return shortURL
+}
+
+function writeToDatabase(data, path) {
+  try {
+    fs.writeFileSync(path, JSON.stringify(data, null, 2), 'utf8')
+    console.log('JSON file has been written successfully.')
+    
+  } catch (err) {
+    console.error('Error writing JSON file:', err)
+  }
 }
